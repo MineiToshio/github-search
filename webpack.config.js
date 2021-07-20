91c0aacc44712215ce61764fcc02c2e486ecfee4
Toshio Minei <sminei10@gmail.com>
2021-07-20 15:11:31 -0500

    chore: setup react and typescript

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [{
+      test: /\.(js|ts|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+        }
+      }
+    }]
+  },
+  devServer: {
+    port: 3000,
+  }
+}
